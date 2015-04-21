d25d8f672b00321cdde389bb4e573296f4180918
rahuang <rahuang@andrew.cmu.edu>
2015-04-21 18:54:58 -0400

    Demo

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import requests
+
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
+    return HttpResponse('Hello from Python Test1!')
 
 
 def db(request):
